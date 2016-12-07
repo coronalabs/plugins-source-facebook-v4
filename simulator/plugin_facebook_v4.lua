5494eb7a013d2e0226d44acdfffc404cb2557f78
Ajay McCaleb <ajay@coronalabs.com>
2016-12-07 10:56:25 -0800

    Facebook-v4: API implementations in the Corona Simulator stub now return values matching the return types advertised in the documentation.
    Reviewed by: Sergey
    
    --HG--
    branch : fb-build-from-source

--- simulator/plugin_facebook_v4.lua
+++ simulator/plugin_facebook_v4.lua
@@ -8,20 +8,36 @@ local function defaultFunction()
 	print( "WARNING: The '" .. lib.name .. "' library is not available in the Corona Simulator." )
 end
 
+local function defaultFunctionReturnNil()
+	defaultFunction()
+	return nil
+end
+
+local function defaultFunctionReturnFalse()
+	defaultFunction()
+	return false
+end
+
+local function defaultFunctionReturnTrue()
+	defaultFunction()
+	return true
+end
+
 lib.login = defaultFunction
 lib.logout = defaultFunction
 lib.request = defaultFunction
 lib.showDialog = defaultFunction
 lib.publishInstall = defaultFunction
-lib.accessDenied = defaultFunction
+lib.accessDenied = defaultFunctionReturnTrue
 
 -- New Facebook-v4 APIs
-lib.getCurrentAccessToken = defaultFunction
-lib.isActive = defaultFunction
+lib.getCurrentAccessToken = defaultFunctionReturnNil
+lib.isActive = defaultFunctionReturnTrue
 lib.setFBConnectListener= defaultFunction
+lib.isFacebookAppEnabled = defaultFunctionReturnFalse
 
 -- Introduced in Android Beta v1 and deprecated later
-lib.currentAccessToken = defaultFunction
+lib.currentAccessToken = defaultFunctionReturnNil
 
 -- Return an instance
 return lib
